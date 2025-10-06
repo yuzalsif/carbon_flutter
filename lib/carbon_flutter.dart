fcd278b8008f294dbd415bbd06f6fd742f6a33b2
yuzalsif <yuseifabdillah@gmail.com>
2025-10-06 17:50:33 +0300

    Expose carbon colors

--- lib/carbon_flutter.dart
+++ lib/carbon_flutter.dart
@@ -1,5 +1,13 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+/// A Flutter implementation of the IBM Carbon Design System.
+///
+/// This library provides a set of widgets and themes to build applications
+/// with the Carbon look and feel. To use the components, import this file.
+///
+/// ```dart
+/// import 'package:carbon_flutter/carbon_flutter.dart';
+/// ```
+library carbon_flutter;
+
+// --- Foundation ---
+// Exporting the color definitions so they can be used publicly.
+export 'src/theme/colors.dart';
